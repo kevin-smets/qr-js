4cc1f70709ecf58f291aea67852901955b144a9d
Kevin Smets <kevin.smets2@kbc.be>
2016-05-26 21:53:30 +0200

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # qrjs
-Generate QR codes from the command line, optionally save them straight to file
+
+Generate QR codes from the command line. Output is either base64 or an html snippet.
+
+You can also save the output straight to an image file.
+
+# Install
+
+```
+npm install --global qrjs
+```
+
+# Use
+
+```
+qrjs --help
+```
+
+Output to command line:
+
+```
+qrjs http://github.com/kevin-smets
+```
+
+Output html snippet to command line:
+
+```
+qrjs http://github.com/kevin-smets --html
+```
+
+Output to file (.gif):
+
+```
+qrjs http://github.com/kevin-smets -o fileName
+```
---
+++ bin/qrjs.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+'use strict';
+
+var base64Img = require('base64-img');
+var qrcode = require('yaqrcode');
+var path = require('path');
+
+var argv = require('yargs')
+  .usage('qrjs <data> [-o fileName] [--html]')
+  .option('output', {
+    alias: 'o',
+    describe: 'Output to file',
+    type: 'string'
+  })
+  .option('html', {
+    alias: 'o',
+    describe: 'Output as html',
+    type: 'boolean'
+  })
+  .help('help')
+  .version()
+  .argv;
+
+var input = argv._[0];
+
+if (typeof input !== "string") {
+  console.log("I need something to work with, e.g. 'qrjs http://github.com/kevin-smets'")
+  process.exit(1);
+}
+
+var qrOut = qrcode(input);
+
+if (argv.o) {
+  var fileName = path.basename(argv.o, '.gif')
+  base64Img.imgSync(qrOut, '', fileName)
+  console.log("QR code saved to file " + fileName + ".gif");
+} else {
+  if (argv.html) {
+    console.log('<img src="' + qrOut + '" alt="" />');
+  } else {
+    console.log(qrOut);
+  }
+}
